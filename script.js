0ab60001a01d0ebf04610fdffda4bf37c98090c8
Shah Muhammad Jannatul Hasan <shahmdjhm@gmail.com>
2023-03-07 23:44:11 +0600

    Playing around

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ var myChart = new Chart(ctx, {
         maintainAspectRatio: false,
     }
 });
-var ctx = document.getElementById("ctx2").getContext('2d');
+var ctx2 = document.getElementById("ctx2").getContext('2d');
 
-var myChart = new Chart(ctx, {
+var myChart2 = new Chart(ctx2, {
     type: 'doughnut',
     data: {
         labels: ["Laufzeit", "Standby"],
@@ -34,3 +34,23 @@ var myChart = new Chart(ctx, {
         maintainAspectRatio: false,
     }
 });
+const setVal = (num) => {
+    const value = parseInt(num);
+    myChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ["Laufzeit", "Standby"],
+            datasets: [{
+                data: [value, 50],
+                borderColor: ['#52b1ff', '#82ff87'],
+                backgroundColor: ['#52b1ff', '#82ff87'],
+                borderWidth: 0
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+        }
+    });
+}
+console.log(document.querySelector("input[type='radio'][name='stair']:checked").value)
